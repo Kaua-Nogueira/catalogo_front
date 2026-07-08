4c26a460038343657d5961fe8ebba03320e79304
Kaua-Nogs <kauanog9@gmail.com>
2026-07-07 21:23:35 -0300

    fix: integrate nitro plugin to generate .output build directory

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsConfigPaths from "vite-tsconfig-paths";
+import { nitro } from "nitro/vite";
 
 export default defineConfig({
   plugins: [
@@ -10,6 +11,9 @@ export default defineConfig({
     tsConfigPaths({ projects: ["./tsconfig.json"] }),
     tanstackStart({
       server: { entry: "server" },
+    }),
+    nitro({
+      preset: "node-server"
     }),
     viteReact(),
   ],
@@ -19,3 +23,4 @@ export default defineConfig({
     }
   }
 });
+
